c57de553d6d20894810219d4728d39b07f3a8d9a
Travis CI User <travis@example.org>
2019-08-29 23:31:49 +0000

    Automatic Travis documentation build

---
+++ master/implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["secrets"] = [{text:"impl&lt;T&gt; Freeze for <a class=\"struct\" href=\"secrets/struct.Secret.html\" title=\"struct secrets::Secret\">Secret</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: Freeze,&nbsp;</span>",synthetic:true,types:["secrets::secret::Secret"]},{text:"impl&lt;T&gt; !Freeze for <a class=\"struct\" href=\"secrets/struct.SecretBox.html\" title=\"struct secrets::SecretBox\">SecretBox</a>&lt;T&gt;",synthetic:true,types:["secrets::secret_box::SecretBox"]},{text:"impl&lt;T&gt; !Freeze for <a class=\"struct\" href=\"secrets/struct.SecretVec.html\" title=\"struct secrets::SecretVec\">SecretVec</a>&lt;T&gt;",synthetic:true,types:["secrets::secret_vec::SecretVec"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ master/implementors/std/panic/trait.UnwindSafe.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["secrets"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"secrets/struct.Secret.html\" title=\"struct secrets::Secret\">Secret</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a>,&nbsp;</span>",synthetic:true,types:["secrets::secret::Secret"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"secrets/struct.SecretBox.html\" title=\"struct secrets::SecretBox\">SecretBox</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a>,&nbsp;</span>",synthetic:true,types:["secrets::secret_box::SecretBox"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"secrets/struct.SecretVec.html\" title=\"struct secrets::SecretVec\">SecretVec</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a>,&nbsp;</span>",synthetic:true,types:["secrets::secret_vec::SecretVec"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
